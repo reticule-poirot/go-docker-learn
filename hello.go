126a41f053461085b6b4428ced78b017c159e637
reticule-poirot <56135698+reticule-poirot@users.noreply.github.com>
2026-08-07 03:11:05 +0300

    rewrite hello.go

--- hello.go
+++ hello.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
-func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/", HomeHandler)
-	log.Fatal(http.ListenAndServe(":8000", router))
+var port int
+
+func health(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ok"))
 }
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	hostname := os.Getenv("HOSTNAME")
 	log.Printf("Hello, I'm %v", hostname)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(fmt.Sprintln("Hello, I'm", hostname, "!")))
+}
+
+func main() {
+
+	flag.IntVar(&port, "port", 8000, "an int")
+	flag.Parse()
+
+	log.Printf("Port: %v", port)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", home).Methods(http.MethodGet)
+	router.HandleFunc("/health", health).Methods(http.MethodGet)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), router))
 }
